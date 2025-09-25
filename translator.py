fe2825c32bd7c884c90daedd9732ca051f1ace87
P-Sanjana <podduturisanjana@gmail.com>
2025-09-25 17:55:16 -0400

    added languages list

--- translator.py
+++ translator.py
@@ -1,11 +1,7 @@
 import streamlit as st
 from langchain_google_genai import GoogleGenerativeAI
-from dotenv import load_dotenv
-import os
-
-load_dotenv()
-
-llm = GoogleGenerativeAI(model='gemini-2.5-flash', api_key=os.environ['GEMINI_API_KEY'])
+from languages_list import languages
+llm = GoogleGenerativeAI(model='gemini-2.5-flash', api_key=st.secrets['GEMINI_API_KEY'])
 
 st.title('AI Translator')
 
@@ -13,8 +9,8 @@ st.divider()
 
 input_text = st.text_area('Enter the text you want to translate:')
 
-source_language = st.selectbox('Select source language:', ['English', 'Spanish', 'French', 'German'])
-target_language = st.selectbox('Select target language:', ['English', 'Spanish', 'French', 'German'])
+source_language = st.selectbox('Select source language:', languages)
+target_language = st.selectbox('Select target language:', languages)
 
 def translate_text(input_text, source_language, target_language):
     prompt = f'Translate this from {source_language} to {target_language}: {input_text}'
